4b85e6ccd6185b014398dd2394c6ac382850964f
AnantPadwal <111486698+AnantPadwal@users.noreply.github.com>
2022-08-19 09:58:04 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>DIILLI DARBAR TIMES</title>
+</head>
+<body>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap');
+
+</style>
+   <h1 class="mx-auto d-block" align="center" style="font-family: 'Black Ops One'">  <img  src="https://cdn.discordapp.com/attachments/1009475935319371816/1009709832837808128/black_on_trans.png" alt="logo" width="150" height="150">DIILLI DARBAR TIMES</h1>      
+<nav class="navbar sticky-top navbar-expand-sm bg-dark navbar-dark mb-3" >
+  <ul class="navbar-nav mx-auto">
+    <li class="nav-item mr-3">
+      <a class="nav-link" href="index.php"><h4>Home</h4></a>
+    </li>
+    <li class="nav-item mr-3">
+      <a class="nav-link" href="sports.php"><h4> Sports</h4></a>
+    </li>
+    <li class="nav-item mr-3">
+      <a class="nav-link" href="bollywood.php"><h4> Bollywood</h4></a>
+    </li>
+    <li class="nav-item mr-3">
+      <a class="nav-link" href="daffairs.php"><h4> Daily_Affairs</h4></a>
+    </li>
+    <li class="nav-item mr-3">
+      <a class="nav-link" href="international.php"><h4> International</h4></a>
+    </li>
+    <li class="nav-item mr-3">
+      <a class="nav-link" href="finance.php"><h4> Finanace</h4></a>
+    </li>
+    <li class="nav-item mr-3">
+      <a class="nav-link" href="achievement.php"><h4> Achievements</h4></a>
+    </li>
+    <li class="nav-item active mr-3">
+      <a class="nav-link" href="aboutus.php"><h4> AboutUs</h4></a>
+    </li>
+  </ul>
+  
+  <h1><div id="google_element"></div></h1>
+  <script src="http://translate.google.com/translate_a/element.js?cb=loadGoogleTranslate">
+  </script>
+  <script>
+    function loadGoogleTranslate() {
+      new google.translate.TranslateElement({
+        defaultLanguage: 'en',
+        pageLanguage: 'en',
+        includedLanguages: 'en,hi'
+      },
+        "google_element"
+      );
+    }
+  </script>
+</nav>
+
+    
+    <div class="card card-body bg-light text-dark" style="height:600px; width:1100px; margin-left:133px; padding-top:25px; padding-bottom:25px;">
+    <img src="img/information-button.png" class="rounded mx-auto d-block" style="height:450px; width:500px;" alt=""><br>
+   <h5 class="mx-auto d-block" style="text-align: justify;">The origins of DIILLI DARBAR TIMES, the North region's largest circulated English as well as Hindi broadsheet newspaper, go way back to 1950. At present, it is the nation's first English newspaper of choice with 55 editions which cater to millions of readers across the Delhi.</h5>
+      
+    
+    </div><br><br>
+    
+      
+    
+    </div><br><br>
+
+</body>
+
+</html>
